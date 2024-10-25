ada4f6b8f13db0b18588b3a180326066b85cb23b
Ori-Apis <56266958+Ori-Apis@users.noreply.github.com>
2024-10-25 11:33:09 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,37 @@
 function love.load()
 
   HC = require 'lib/HC'
-  mouse = HC.rectangle(300,400,40,40)
-  mouse:moveTo(love.mouse.getPosition())
+
+  player = {}
+
+  player.x = 200
+  player.y = 100
+
+  player.speed = 5
+  player.width = 100
+  player.height = 100
+  player.hitbox = HC.rectangle(300,400,40,40)
+  
 end
 
 function love.update(dt)
+
+  if love.keyboard.isDown('left') then
+    player.x = player.x - player.speed
+  end
+
+  if love.keyboard.isDown('right') then
+    player.x = player.x + player.speed
+  end
+
+  if love.keyboard.isDown('up') then
+    player.y = player.y - player.speed
+  end
+
+  if love.keyboard.isDown('down') then
+    player.y = player.y + player.speed
+  end
   
-  mouse:moveTo(love.mouse.getPosition())
 end
 
 function love.draw()
